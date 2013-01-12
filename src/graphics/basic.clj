32480799de8a5decc7edbb19de096977820f6e49
Mark Feeney <mark.feeney@gmail.com>
2013-01-11 23:21:16 -0500

    Dynamically change images in a frame
    Less dumb than before.  Shows a buffered image in a frame.  Reuses the
    frame, but updates its components when there's a new image to show.
    Also some code to generate images with a coloured line on them.

--- src/graphics/basic.clj
+++ src/graphics/basic.clj
@@ -1,23 +1,40 @@
-(ns graphics.basic
-  (:import (javax.swing JFrame JPanel)))
+(ns ^{:doc "A simple example app of displaying a generated image in swing"
+      :author "Mark Feeney"}
+  graphics.basic
+  (:import (javax.swing ImageIcon JFrame JLabel JPanel)
+           (java.awt Color)
+           (java.awt.image BufferedImage)))
 
-(defonce draw-fn (atom nil))
+(set! *warn-on-reflection* true)
 
-(defn draw [g]
-  (.drawLine g 10 10 20 20))
+;; "A global frame that we can put images on."
+(defonce frame
+  (doto (JFrame. "Basic graphics")
+    (.setDefaultCloseOperation(JFrame/DISPOSE_ON_CLOSE))))
 
-(reset! draw-fn draw)
+(defn display-image 
+  "Display img on frame, replacing whatever is currently there."
+  [^JFrame frame ^BufferedImage img]
+  (.. frame (getContentPane) (removeAll))
+  (.add frame (JLabel. (ImageIcon. img)))
+  (if (not (.isVisible frame))
+    (.setVisible frame true)
+    (.validate frame)))
 
-(defn make-panel []
-  (proxy [JPanel] []
-    (paintComponent [g]
-      (proxy-super paintComponent g)
-      (@draw-fn g))))
 
-(defn make-frame []
-  (doto (JFrame. "Basic graphics example")
-    (.setSize 500 500)
-    (.setContentPane (make-panel))))
+(comment
 
-(defn test []
-  (doto (make-frame) (.pack) (.setVisible true)))
+(defn make-test-image [^Color col]
+  (let [img (BufferedImage. 300 200 BufferedImage/TYPE_INT_ARGB)]
+    (doseq [x (range 300)
+            y (range 10)]
+      (.setRGB img x (+ y 30) (.getRGB col)))
+    img))
+
+(defn make-red [] (make-test-image Color/RED))
+(defn make-blue [] (make-test-image Color/BLUE))
+(display-image frame (make-red))
+(display-image frame (make-blue))
+
+)
+
