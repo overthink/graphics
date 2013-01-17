a6a917fd1b168f8fb0062bf8cfdaa05618b47081
Mark Feeney <mark.feeney@gmail.com>
2013-01-16 22:35:54 -0500

    Half-baked Mandelbrot

--- src/graphics/basic.clj
+++ src/graphics/basic.clj
@@ -14,7 +14,7 @@
 
 ;; Useful in colour generating functions
 (defonce prng (java.util.Random.))
-(defn rnd [] (.nextFloat prng))
+(defn rnd [] (.nextFloat ^java.util.Random prng))
 
 (defn display-image
   "Display img on frame, replacing whatever is currently there."
@@ -44,11 +44,13 @@
   [width height aafactor f]
   (let [w (* aafactor width)
         h (* aafactor height)
+        xscale 1
+        yscale 1
         img (BufferedImage. w h BufferedImage/TYPE_INT_RGB)]
     (doseq [x (range w)
             y (range h)]
-      (let [scaledx (/ (float x) w)
-            scaledy (/ (float y) h)
+      (let [scaledx (* xscale (/ (float x) w))
+            scaledy (* yscale (/ (float y) h))
             [r g b] (f scaledx scaledy)
             col (Color. (float r) (float g) (float b))]
         (.setRGB img x y (.getRGB col))))
@@ -56,9 +58,28 @@
       img
       (resize-image img width height))))
 
+(defn mandel
+  "Mandelbrot set. http://en.wikipedia.org/wiki/Mandelbrot_set#For_programmers"
+  [x y]
+  (let [max-iters 1000
+        x (float x) ;; 20x speedup from forcing these to primitives
+        y (float y)
+        iters (loop [curx x cury y iter 0]
+                (if (and (< (+ (* curx curx) (* cury cury)) 4)
+                         (< iter max-iters))
+                  (recur (+ (- (* curx curx) (* cury cury)) x)
+                         (+ y (* 2 (* x y)))
+                         (inc iter))
+                  iter))]
+    ;; If it took max-iters, it's a member of the set, else colour the pixel
+    ;; based on the number of iters before escape.
+    (if (= iters max-iters)
+      [0 0 0]
+      [0 0 (min 1 (/ iters 15))])))
+
 (comment
 
-  (defn show [f] (display-image frame (make-image 400 400 2 f)))
+  (defn show [f] (display-image frame (make-image 300 300 2 f)))
 
   ;; Simple gradient
   (defn f1 [x y] [x y 0])
@@ -70,12 +91,18 @@
 
   ;; sin(x)
   (defn sin [x y]
-    (let [sinx (Math/sin x)
+    (let [sinx (* 0.5 (+ 1.0 (Math/sin x)))
           delta (Math/abs (- sinx y))]
-      (if (< delta 0.005)
+      (if (< delta 0.01)
         [0 0 0]
         [1 1 1])))
   (show sin)
+
+  (defn f3 [x y] [x y 0.5])
+  (show f3)
+
+  (defn f4 [x y] [(Math/sin x) (Math/sin y) (Math/sin (+ x y))])
+  (show f4)
 
 )
 
